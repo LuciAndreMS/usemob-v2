99a9f5d52a5d2b034f2f4094545649ad225230d9
LuciAndreMS <luandre2007@gmail.com>
2026-02-27 15:37:59 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Garante que o site fique sempre no tema escuro
+document.documentElement.classList.add("dark");
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);

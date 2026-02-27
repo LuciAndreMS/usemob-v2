80305fc38bcbdc0c1ada137f59203cac770e4104
LuciAndreMS <luandre2007@gmail.com>
2026-02-27 17:55:07 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,14 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Index from "./pages/Index.tsx";
-import NotFound from "./pages/NotFound.tsx";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
 
-// Páginas SEO (com extensão .tsx para evitar falha no build da Vercel)
-import TransporteCorporativoTresLagoas from "./pages/TransporteCorporativoTresLagoas.tsx";
-import MobilidadeCorporativaMS from "./pages/MobilidadeCorporativaMS.tsx";
-import TransporteParaIndustriaMS from "./pages/TransporteParaIndustriaMS.tsx";
-import LocacaoVeiculosComMotorista from "./pages/LocacaoVeiculosComMotorista.tsx";
-import LogisticaEmpresarialMS from "./pages/LogisticaEmpresarialMS.tsx";
+import TransporteCorporativoTresLagoas from "./pages/TransporteCorporativoTresLagoas";
+import MobilidadeCorporativaMS from "./pages/MobilidadeCorporativaMS";
+import TransporteParaIndustriaMS from "./pages/TransporteParaIndustriaMS";
+import LocacaoVeiculosComMotorista from "./pages/LocacaoVeiculosComMotorista";
+import LogisticaEmpresarialMS from "./pages/LogisticaEmpresarialMS";
 
 const queryClient = new QueryClient();
 
@@ -26,7 +25,6 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
 
-          {/* Páginas SEO */}
           <Route
             path="/transporte-corporativo-tres-lagoas"
             element={<TransporteCorporativoTresLagoas />}
@@ -39,7 +37,6 @@ const App = () => (
           />
           <Route path="/logistica-empresarial-ms" element={<LogisticaEmpresarialMS />} />
 
-          {/* Catch-all */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
